d56a40220cfecba324c58a6a88a2b41ff0157182
jiawei1996 <starlight@Jiaweis-MacBook-Pro.local>
2017-04-18 18:14:50 +0800

    pictures lazy-load

--- javascripts/pictures_load.js
+++ javascripts/pictures_load.js
@@ -1,10 +1,16 @@
 (function(){
-	alert(1);
-	$("h3").hide();
-	$(".navigation").length;
-	$(".navigation").each(function(index){
-		alert(1);
-		alert(this);
-	});
-	alert(2);
+	var index = 0;
+	setTimeout(function(){
+		show_pictures();
+		var timeout;
+		function show_pictures(){
+			$("figure").eq(index).fadeTo(600,1);
+			index++;
+			timeout = setTimeout(show_pictures,400);
+			if(index == 5) $("#mainpage_singer").fadeTo(1000,1);
+			if(index > 4) clearTimeout(timeout);
+		}	
+	},2000);
+	
+
 })();
